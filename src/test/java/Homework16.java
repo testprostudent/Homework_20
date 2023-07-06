38ffbcea25f096df2291a3d3ed6c00b2ef749fb2
testprostudent <137521093+testprostudent@users.noreply.github.com>
2023-07-06 14:45:23 -0400

    Homework_20
    INTO

---
+++ src/test/java/Homework16.java
@@ -0,0 +1,8 @@
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class Homework16 extends BaseTest {
+    ChromeOptions options =new ChromeOptions();
+    options.addArguments("remote")
+
+}
